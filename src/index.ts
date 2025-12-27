06780e72c4d21f4cad382e302b079ac3782b9121
unknown <nikita.987.nikita@gmail.com>
2025-12-27 11:03:54 +0200

    Install modules, start server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('Happy developing ✨')
+import express, {Express} from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app: Express = express();
+
+app.listen(process.env.SERVER_PORT, () => {
+    console.log("Server running on port: " + process.env.SERVER_PORT);
+});
